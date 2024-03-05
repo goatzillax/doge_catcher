19cc3479e43ad1385e8b53119100ad0064dd836a
goatzilla <goatzillax@users.noreply.github.com>
2024-03-04 20:32:30 -0600

    i dunno huffyuv files were hilariously large
    also logfiles are broken with dvr-scan

--- dvr-scan.sh
+++ dvr-scan.sh
@@ -6,9 +6,10 @@ DN=`dirname $FN`
 TEMP=`basename $FN .mp4`
 BN=$DN/$TEMP
 
-ffmpeg -y -i $FN -c:v huffyuv $BN-temp.avi
+#ffmpeg -y -i $FN -c:v huffyuv $BN-temp.avi
+ffmpeg -y -i $FN -c:v libx264 -b:v 5000k $BN-temp.avi
 
 #dvr-scan -i temp.avi -a 849 60 1559 78 1549 141 1510 147 1485 199 868 177 -o $BN.avi --logfile $BN.log
-dvr-scan -i $BN-temp.avi -a 859 70 1207 70 1554 100 1552 136 1504 136 1485 191 867 167 -t 0.30 -o $BN.avi --logfile $BN.log
+dvr-scan -i $BN-temp.avi -a 859 70 1207 70 1554 100 1552 136 1504 136 1485 191 867 167 -t 0.30 -o $BN.avi > $BN.log
 
 rm $BN-temp.avi
